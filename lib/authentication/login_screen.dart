bd1adff663c4de53ff7fa884d7bd717e04b46d50
robertotonde <robertotonde316@gmail.com>
2024-01-24 18:00:50 +0300

    block status

--- lib/authentication/login_screen.dart
+++ lib/authentication/login_screen.dart
@@ -1,5 +1,11 @@
+import 'package:firebase_auth/firebase_auth.dart';
+import 'package:firebase_database/firebase_database.dart';
 import 'package:flutter/material.dart';
+import 'package:kp_trans/Widgets/loading_dialog.dart';
 import 'package:kp_trans/authentication/signup_screen.dart';
+import 'package:kp_trans/global/global_var.dart';
+import 'package:kp_trans/methods/common_methods.dart';
+import 'package:kp_trans/pages/home.dart';
 
 class LoiginScreen extends StatefulWidget {
   const LoiginScreen({super.key});
@@ -11,6 +17,66 @@ class LoiginScreen extends StatefulWidget {
 class _LoiginScreenState extends State<LoiginScreen> {
   TextEditingController emailTextEditingController = TextEditingController();
   TextEditingController passwordTextEditingController = TextEditingController();
+
+  CommonMethods cMethods = CommonMethods();
+
+  checkIfNetworkIsAvailable() {
+    cMethods.checkConnectivity(context);
+    loginFormValidation();
+  }
+
+  loginFormValidation() {
+    if (!emailTextEditingController.text.contains('@')) {
+      cMethods.displaySnackBar("provide a valid email", context);
+    } else if (passwordTextEditingController.text.trim().length < 5) {
+      cMethods.displaySnackBar("passwor must be atleast 6 characters", context);
+    } else {
+      signInUser();
+    }
+  }
+
+  signInUser() async {
+    showDialog(
+        context: context,
+        barrierDismissible: false,
+        builder: (BuildContext context) =>
+            LoadingDialog(messageText: "signing in..."));
+
+    final User? userFirebase = (await FirebaseAuth.instance
+            .signInWithEmailAndPassword(
+                email: emailTextEditingController.text.trim(),
+                password: passwordTextEditingController.text.trim())
+            .catchError((errormsg) {
+      Navigator.pop(context);
+      cMethods.displaySnackBar(errormsg.toString(), context);
+    }))
+        .user;
+    if (!context.mounted) return;
+    Navigator.pop(context);
+
+    if (userFirebase != null) {
+      DatabaseReference usersRef = FirebaseDatabase.instance
+          .ref()
+          .child("users")
+          .child(userFirebase.uid);
+      usersRef.once().then((snap) {
+        if (snap.snapshot.value != null) {
+          if ((snap.snapshot.value as Map)["blockstatus"] == "no") {
+            userName = (snap.snapshot.value as Map)["name"];
+            Navigator.push(
+                context, MaterialPageRoute(builder: (c) => HomePage()));
+          } else {
+            FirebaseAuth.instance.signOut();
+            cMethods.displaySnackBar(
+                "user is blocked :contact admin@gmail.com ", context);
+          }
+        } else {
+          FirebaseAuth.instance.signOut();
+          cMethods.displaySnackBar("user does not exist", context);
+        }
+      });
+    }
+  }
 
   @override
   Widget build(BuildContext context) {
@@ -65,7 +131,9 @@ class _LoiginScreenState extends State<LoiginScreen> {
                       height: 22,
                     ),
                     ElevatedButton(
-                      onPressed: () {},
+                      onPressed: () {
+                        checkIfNetworkIsAvailable();
+                      },
                       style: ElevatedButton.styleFrom(
                         backgroundColor: Colors.purple,
                         padding: const EdgeInsets.symmetric(
--- lib/main.dart
+++ lib/main.dart
@@ -1,23 +1,30 @@
 import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
-import 'package:kp_trans/authentication/login_screen.dart';
+
 import 'package:kp_trans/authentication/signup_screen.dart';
 
-void main() {
+
+Future<void> main() async
+{
   WidgetsFlutterBinding.ensureInitialized();
-  Firebase.initializeApp();
+  await Firebase.initializeApp();
+
   runApp(const MyApp());
 }
 
-class MyApp extends StatelessWidget {
+class MyApp extends StatelessWidget
+{
   const MyApp({super.key});
 
   @override
-  Widget build(BuildContext context) {
+  Widget build(BuildContext context)
+  {
     return MaterialApp(
-      title: 'Kp trans',
+      title: 'Flutter Demo',
       debugShowCheckedModeBanner: false,
-      theme: ThemeData.dark().copyWith(scaffoldBackgroundColor: Colors.black),
+      theme: ThemeData.dark().copyWith(
+        scaffoldBackgroundColor: Colors.black,
+      ),
       home: SignupScreen(),
     );
   }
